191e1392968a733a498798fae3fbbadb394af1d8
erfanfi79 <erfanfaravani@gmail.com>
2023-11-06 14:06:54 +0330

    Fix mobile width

--- app/page.js
+++ app/page.js
@@ -10,10 +10,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div class="flex min-h-screen justify-center bg-gradient-to-l from-blue-200 to-stone-300">
-      <div class="fixed top-0 z-10 w-4/5 max-w-5xl shadow-md">
+      <div class="fixed top-0 z-10 w-full sm:w-4/5 max-w-5xl shadow-md">
         <Navbar />
       </div>
-      <div class="flex w-4/5 max-w-5xl flex-col shadow-xl ">
+      <div class="flex w-full sm:w-4/5 max-w-5xl flex-col shadow-xl ">
         <div class="h-20"></div>
         <HeroSection />
         <Experience id="s-experience" />
@@ -23,7 +23,7 @@ export default function Home() {
         <ContactMe id="s-contactme" />
         <div class="h-40"></div>
       </div>
-      <div class="fixed bottom-0 z-10 w-4/5 max-w-5xl shadow-md">
+      <div class="fixed bottom-0 z-10 w-full sm:w-4/5 max-w-5xl shadow-md">
         <Footer />
       </div>
     </div>
