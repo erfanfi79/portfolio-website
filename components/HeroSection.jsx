d8a24ab24a4666926c003239af0b085f2f66bdfd
erfanfi79 <erfanfaravani@gmail.com>
2023-11-10 21:52:02 +0330

    Fix build problems

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -11,9 +11,9 @@ const HeroSection = () => {
       <div class="hero-content text-left text-neutral-content">
         <div class="max-w-3xl">
           <Image width={120} height={120} src={"/boypic.png"} alt="portfolio-hero-pic" />
-          <h1 class="mb-5 mt-4 text-4xl font-bold">Hello, I’m Erfan</h1>
+          <h1 class="mb-5 mt-4 text-4xl font-bold">{"Hello, I’m Erfan"}</h1>
           <p class="mb-5">
-          I'm a Full-Stack Developer with a keen interest in Blockchain Technology. With over two year of hands-on experience, I've developed a strong skill set in a variety of technologies, including Solidity, Django, Golang, React, and Next.js.
+          {"I'm a Full-Stack Developer with a keen interest in Blockchain Technology. With over two year of hands-on experience, I've developed a strong skill set in a variety of technologies, including Solidity, Django, Golang, React, and Next.js."}
           </p>
           {/* <button class="btn border-hidden bg-blue-800 text-white hover:text-black">
             See Projects
