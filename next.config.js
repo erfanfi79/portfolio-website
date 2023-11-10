97530fbc77a6cbf251060678b0e4b2c37feb01b7
erfanfi79 <erfanfaravani@gmail.com>
2023-11-10 23:38:44 +0330

    add basepath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = '/'
+
 
 const nextConfig = {
     output: 'export',
-
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 }
 
 module.exports = nextConfig
